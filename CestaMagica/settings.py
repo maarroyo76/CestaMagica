c7a551036c7324a02b1d5dfaa687c6efa582ef31
Martin Arroyo <ma.arroyoq@duocuc.cl>
2025-01-28 17:43:34 -0300

    fix storages

--- CestaMagica/settings.py
+++ CestaMagica/settings.py
@@ -159,6 +159,7 @@ AWS_S3_FILE_OVERWRITE = False
 STORAGES = {
     'default': {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "LOCATION": "media"
     },
     'staticfiles': {
         'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
